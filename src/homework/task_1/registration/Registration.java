ca6e347d4ce5ed5d6df3e13911a7ea2e01bcd902
ddumaa <dimaduma62@gmail.com>
2023-12-14 19:18:45 +0300

    fix

--- src/homework/task_1/registration/Registration.java
+++ src/homework/task_1/registration/Registration.java
@@ -7,10 +7,11 @@ public class Registration {
     static int length = 20;
     public static boolean newUser (String login, String password, String confirmPassword) throws Exception {
         if (login.length() > length || login.contains(" ")){
-            throw new WrongLoginException(1, "the login does not match the required parameters, it is more than 20 characters or has spaces");
+            throw new WrongLoginException(10, "the login does not match the required parameters, it is more than 20 characters or has spaces");
         }
+        //изначально использовал данную часть !password.matches("^\\d*$") - и она не работала
         if (password.length() > length || password.contains(" ") || password.matches("^\\D*$")){
-            throw new WrongPasswordException(2, "the password does not meet the required parameters, it is more than 20 characters, does not have numbers or has spaces.");
+            throw new WrongPasswordException(20, "the password does not meet the required parameters, it is more than 20 characters, does not have numbers or has spaces.");
         }
         if (!confirmPassword.equals(password)){
             throw new Exception ("Password mismatch");
